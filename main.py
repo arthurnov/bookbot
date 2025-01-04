64ea583bcd4e55c36c861a265c92900934a00175
arthurnov <art.dsky@gmail.com>
2025-01-04 19:31:33 +0200

    added letter counter

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letters = get_letter_count(text)
     print(f"{num_words} words found in the document")
+    print(letters)
 
+def get_letter_count(text):
+    lower_case = text.lower()
+    letters={}
+    for letter in lower_case:
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+        
+    return(letters)
 
 def get_num_words(text):
     words = text.split()
